5d67744db7e61fa0691e4cc2f997d464c4dae9c4
chinnaragamsetti <ragamsettirajkumar@gmail.com>
2024-01-01 14:39:37 +0000

    allchanges

---
+++ src/components/GoogleSuggestions/index.js
@@ -0,0 +1,54 @@
+// Write your code
+import {Component} from 'react'
+import SuggestionItem from '../SuggestionItem'
+import './index.css'
+
+class GoogleSuggestions extends Component {
+  state = {searchInput: ''}
+
+  onChangeinputsearch = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  selectlist = suggestion => {
+    this.setState({searchInput: suggestion})
+  }
+
+  render() {
+    const {suggestionsList} = this.props
+    const {searchInput} = this.state
+    const aftersearchedlist = suggestionsList.filter(eachlist =>
+      eachlist.name.toLowercase().includes(searchInput),
+    )
+    return (
+      <div className="maincontainer">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+          className="googlelogo"
+          alt="google logo"
+        />
+        <div className="subcontainer">
+          <div className="inputbarcontainer">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+              className="searchicon"
+              alt="search icon"
+            />
+            <input type="search" onChange={this.onChangeinputsearch} />
+          </div>
+          <ul className="listcontainer">
+            {aftersearchedlist.map(eachsuggestion => (
+              <SuggestionItem
+                key={eachsuggestion.id}
+                suggestiondetails={eachsuggestion}
+                selectlist={this.selectlist}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GoogleSuggestions
---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,27 @@
+// Write your code here
+
+import './index.css'
+
+const SuggestionItem = props => {
+  const {eachsuggestion, selectlist} = props
+
+  const {suggestion} = eachsuggestion
+
+  const onSelect = () => {
+    selectlist(suggestion)
+  }
+  return (
+    <li className="listitem">
+      <p className="heading">{suggestion}</p>
+      <button type="button" className="button" onClick={onSelect}>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+          alt="arrow"
+          className="arrow"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default SuggestionItem
